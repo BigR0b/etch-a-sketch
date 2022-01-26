63ae39a5f9b1e27947e1d584fe5cc48ca23fe569
Robin Rhodes <bigr0b@protonmail.com>
2022-01-26 00:02:53 -0500

    Finished the grayscale button and made the rainbout button event listner only activate once

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let blockAmount = girdSize ** 2;
 let gridRow = sketchContainer.style.gridTemplateRows = `repeat(${girdSize}, 1fr)`;
 let gridColumn = sketchContainer.style.gridTemplateColumns = `repeat(${girdSize}, 1fr)`;
 
-
 function addTiles(blockAmount, penColor) {
     for (let i = 0; i < blockAmount; i++) {
         let tile = document.createElement("div");
@@ -14,7 +13,7 @@ function addTiles(blockAmount, penColor) {
         sketchContainer.appendChild(tile);
     }
 }
-addTiles(blockAmount, grayscalePen);
+addTiles(blockAmount, blackPen);
 
 function deleteTiles() {
     while (sketchContainer.firstChild) {
@@ -35,36 +34,40 @@ function rainbowPen() {
     let b = Math.floor(Math.random() * 255);
     let rgb = `rgb(${r},${g},${b})`;
     this.style.backgroundColor = rgb;
+    this.removeEventListener("mouseover", rainbowPen);
 }
 function grayscalePen() {
-    let opacity = .1;
     this.style.backgroundColor = "black";
     if (this.style.opacity) {
-        this.style.opacity = opacity +.1;
+        let opacityPercent = parseFloat(this.style.opacity);
+        this.style.opacity = opacityPercent + .1;
     }else{
-        this.style.opacity = opacity;
+        this.style.opacity = .1;
     }
 }
-//Rainbow Button
-const rainbowButton = document.getElementById("rainbow");
-rainbowButton.addEventListener("click" ,() => {
+function clearTable() {
     deleteTiles();
     girdSize = girdSize;
     blockAmount = girdSize ** 2;
     gridRow = sketchContainer.style.gridTemplateRows = `repeat(${girdSize}, 1fr)`;
     gridColumn = sketchContainer.style.gridTemplateColumns = `repeat(${girdSize}, 1fr)`;
+}
+//Rainbow Button
+const rainbowButton = document.getElementById("rainbow");
+rainbowButton.addEventListener("click" ,() => {
+    clearTable()
     addTiles(blockAmount, rainbowPen);
 });
 //Grayscale Button
-
+const grayscaleButton = document.getElementById("grayscale");
+grayscaleButton.addEventListener("click" ,() => {
+    clearTable()
+    addTiles(blockAmount, grayscalePen);
+});
 
 //reset button
 function reset() {
-    deleteTiles();
-    girdSize = prompt("chose");
-    blockAmount = girdSize ** 2;
-    gridRow = sketchContainer.style.gridTemplateRows = `repeat(${girdSize}, 1fr)`;
-    gridColumn = sketchContainer.style.gridTemplateColumns = `repeat(${girdSize}, 1fr)`;
+    clearTable()
     addTiles(blockAmount, blackPen);
 }
 
